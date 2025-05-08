2c4c21ed4c51b06848c51b51cbb56e2b8dcded5c
ChacoFon <zxc90315zxc10315123@gmail.com>
2025-05-08 20:47:57 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import HomePage from "./HP/HomePage";
+import LeyaTalks from "./AP/LeyaTalks";
 
+//主要Return
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentPage, setCurrentPage] = useState("home");
+
+  const handleNavigation = (path) => {
+    if (path === "/LeyaTalks") {
+      setCurrentPage("leyaTalks");
+    } else {
+      setCurrentPage("home");
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {currentPage === "home" ? (
+        <HomePage handleNavigation={handleNavigation} />
+      ) : (
+        <LeyaTalks currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
