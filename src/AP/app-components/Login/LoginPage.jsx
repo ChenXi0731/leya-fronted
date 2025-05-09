ab7b6a6264dce2682a670231bb378b008329ba2e
ChacoFon <zxc90315zxc10315123@gmail.com>
2025-05-09 11:30:28 +0800

    修改登入畫面Icon

--- src/AP/app-components/Login/LoginPage.jsx
+++ src/AP/app-components/Login/LoginPage.jsx
@@ -43,7 +43,7 @@ function LoginPage({ activePage, setActivePage, setUserInfo }) {
 
     return (
         <div className="login-container">
-            <img src="/leya-fronted/src/AP/app-components/assets/images/LeyaLogo.png" alt="" className="login-logo" />
+            <img src="https://raw.githubusercontent.com/ChenXi0731/leya-fronted/refs/heads/main/public/leyalogo.png" alt="" className="login-logo" />
             <h2 className="login-title">樂壓Talks</h2>
             <form onSubmit={handleLogin} className="input-group">
                 <input
