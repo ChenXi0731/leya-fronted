074fa46ead75932f5c683c3111da2794391755bf
ChacoFon <zxc90315zxc10315123@gmail.com>
2025-05-12 22:08:35 +0800

    PWA design update

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { registerServiceWorker, setupPWAInstallPrompt } from './registerSW'
+
+// 注册Service Worker
+registerServiceWorker();
+
+// 设置PWA安装提示
+setupPWAInstallPrompt();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
